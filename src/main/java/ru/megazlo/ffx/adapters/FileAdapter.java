9c419f41901240ffa0411e144556e41cb27b9a28
paradoxfm <paradoxfm@mail.ru>
2016-04-06 20:09:34 +0500

    add fontAwesome.otf

---
+++ src/main/java/ru/megazlo/ffx/adapters/FileAdapter.java
@@ -0,0 +1,17 @@
+package ru.megazlo.ffx.adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * @author iv - 04.04.2016
+ */
+public class FileAdapter extends ArrayAdapter {
+
+
+	public FileAdapter(Context context, int textViewResourceId) {
+		super(context, textViewResourceId);
+	}
+
+
+}
